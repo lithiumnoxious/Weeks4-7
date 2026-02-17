cd3386cfa4ee86cc8acfd13874247f965b506653
zhang278 <zhang278@OA-S141-23>
2026-02-17 10:49:27 -0500

    unity events

---
+++ Assets/script/contact sense.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class contactsense : MonoBehaviour
+{
+    public SpriteRenderer hazard;
+    public bool isinhazard = false;
+    public UnityEvent enteredHazard;
+    public UnityEvent exitHazard;
+
+    public UnityEvent<float> rando;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player in hazard
+        if (hazard.bounds.Contains(transform.position))
+        {
+            if (isinhazard == true)
+            {
+                //still in the hazard
+            }
+            else
+            {
+                //first frame to have entered hazard
+                isinhazard=true;
+                Debug.Log("entered hazard");
+                enteredHazard.Invoke();
+            }
+        }
+        else
+        {
+            if (isinhazard == true)
+            {
+                //first fame out of the hazard
+                isinhazard=false;
+                Debug.Log("exited hazard");
+                exitHazard.Invoke();
+                rando.Invoke(Random.Range(2, 6));
+            }
+        }
+        //yes or no
+    }
+
+    public void shownum(float number)
+    {
+        Debug.Log(number);
+    }
+}
